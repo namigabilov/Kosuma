38edd578cb1031ced2cd46fbc3530eced0e8c26a
namigabilov <namigna@code.edu.az>
2024-11-10 16:26:26 +0400

    Added Datas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,9 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Kosuma.Models;
 using Kosuma.Db;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.SignalR.Protocol;
 using Microsoft.AspNetCore.SignalR;
 using Kosuma.Hubs;
+using Kosuma.Models;
 
 namespace Kosuma.Controllers;
 
@@ -44,6 +42,54 @@ public class HomeController : Controller
                 return RedirectToAction("NotFound");
 
             return View(stream);
+        }
+    }
+
+    public async Task<IActionResult> AddStream()
+    {
+        using (var context = new AppDbContext())
+        {
+            var streams = await context.LiveStreams.Include(c => c.Chats).ToListAsync();
+            context.LiveStreams.RemoveRange(streams);
+
+            List<LiveStream> s = new List<LiveStream>();
+
+            s.Add(new LiveStream
+            {
+                Description = "Embark on a cosmic adventure as we explore the mysteries of the universe, from swirling galaxies to the outer reaches of our solar system. Get ready to have your mind blown by the wonders of space!",
+                Name = "Journey Through the Cosmos 🌌",
+                LiveUrl = "",
+                Thumbnail = "https://img.freepik.com/free-photo/creative-crystal-lens-ball-photography-lake-with-greenery-around-dawn_181624-29379.jpg"
+            });
+
+            s.Add(new LiveStream
+            {
+                Description = "Join us for a taste of global cuisines as we travel from street food in Bangkok to fine dining in Paris—all from your screen! Learn cooking tips, unique recipes, and cultural food stories that bring the world to your kitchen.",
+                Name = "Culinary World Tour: Flavors Across Continents 🍲",
+                LiveUrl = "",
+                Thumbnail = "https://t3.ftcdn.net/jpg/07/25/01/68/360_F_725016819_jOXJkOJZqRSEkdKhheFsLkxuYq59iwa0.jpg"
+            });
+
+            s.Add(new LiveStream
+            {
+                Description = "Delve into the most intriguing mysteries, unsolved cases, and paranormal tales! From famous historical enigmas to lesser-known cases, join the discussion, share theories, and try to crack the mysteries of our world.",
+                Name = "Mystery Hour: Dive into Unsolved Cases 🕵️‍♀️",
+                LiveUrl = "",
+                Thumbnail = "https://img.freepik.com/free-photo/mythical-video-game-inspired-landscape-with-mountains_23-2150974373.jpg"
+            });
+
+            s.Add(new LiveStream
+            {
+                Description = "Relax and unwind as we dive into a fun, beginner-friendly painting session. Bring your own supplies, follow along, or just watch the art unfold! Perfect for artists, beginners, or anyone looking for a creative escape.",
+                Name = "The Creative Hour: Painting with a Twist 🎨",
+                LiveUrl = "",
+                Thumbnail = "https://www.shutterstock.com/image-photo/awesome-pic-natureza-600nw-2408133899.jpg"
+            });
+
+            await context.LiveStreams.AddRangeAsync(s);
+            await context.SaveChangesAsync();
+
+            return Ok("Added !");
         }
     }
 
--- obj/Debug/net8.0/Kosuma.AssemblyInfo.cs
+++ obj/Debug/net8.0/Kosuma.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kosuma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c36eac5305330662159ffcab58ca519a2014772")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kosuma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kosuma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
